ba54a4d92af66c42d0c5cc623b4731b2effae762
Murakami Hiroyuki <h.murakami@esm.co.jp>
2026-02-16 18:07:12 +0900

    feat: ユーザー詳細ダイアログ内のフォーム要素とボタンのUIスタイルを調整しました。

--- packages/frontend/src/components/UserDetailDialog.vue
+++ packages/frontend/src/components/UserDetailDialog.vue
@@ -73,12 +73,20 @@ const handleBeforeClose = (value: boolean) => {
 <template>
   <v-dialog :model-value="modelValue" @update:model-value="handleBeforeClose" max-width="500px">
     <v-card v-if="user">
-      <v-card-title>ユーザー設定</v-card-title>
-      <v-card-text>
+      <v-card-title class="pa-8 pb-0">ユーザー設定</v-card-title>
+      <v-card-text class="pa-8">
         <v-form>
-          <v-row dense>
+          <v-row>
             <v-col cols="12">
-              <v-text-field label="メールアドレス" :model-value="user.email" readonly variant="filled" />
+              <v-text-field
+                label="メールアドレス"
+                :model-value="user.email"
+                readonly
+                variant="filled"
+                density="default"
+                hide-details
+                class="mb-3"
+              />
             </v-col>
             <v-col cols="12">
               <v-text-field
@@ -86,13 +94,25 @@ const handleBeforeClose = (value: boolean) => {
                 :model-value="toDateString(user.attribute.lastLoginAt, 'YYYY/MM/DD HH:mm:ss')"
                 readonly
                 variant="filled"
+                density="default"
+                hide-details
+                class="mb-3"
               />
             </v-col>
             <v-col cols="12">
-              <v-text-field v-model="localDisplayName" label="表示名" autofocus autocomplete="off" />
+              <v-text-field
+                v-model="localDisplayName"
+                label="表示名"
+                autofocus
+                autocomplete="off"
+                variant="outlined"
+                density="compact"
+                hide-details
+                class="mb-3"
+              />
             </v-col>
             <v-col cols="12">
-              <v-radio-group v-model="localTheme" inline label="テーマ" hide-details>
+              <v-radio-group v-model="localTheme" inline label="テーマ" hide-details class="mb-3">
                 <v-radio
                   v-for="option in themeOptions"
                   :key="option.value"
@@ -104,10 +124,10 @@ const handleBeforeClose = (value: boolean) => {
           </v-row>
         </v-form>
       </v-card-text>
-      <v-card-actions>
+      <v-card-actions class="pa-8 pt-0">
         <v-spacer></v-spacer>
-        <v-btn color="blue-darken-1" variant="text" @click="close"> キャンセル </v-btn>
-        <v-btn color="blue-darken-1" variant="text" @click="save"> 保存 </v-btn>
+        <v-btn color="grey-darken-1" variant="text" @click="close"> キャンセル </v-btn>
+        <v-btn color="primary" variant="flat" @click="save" class="ml-2"> 保存 </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
