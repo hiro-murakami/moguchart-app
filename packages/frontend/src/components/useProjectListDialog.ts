6f428b0ec35a039cdae62c742e1179ed158791a4
Murakami Hiroyuki <h.murakami@esm.co.jp>
2026-02-17 18:33:48 +0900

    feat: プロジェクトリストダイアログの列ヘッダーを更新し、プロジェクト列のタイトルを「プロジェクト名」に変更し、期間と操作列に幅を追加

--- packages/frontend/src/components/useProjectListDialog.ts
+++ packages/frontend/src/components/useProjectListDialog.ts
@@ -57,10 +57,10 @@ export const useProjectListDialog = (
   )
 
   const headers = [
-    { title: 'プロジェクト', key: 'project' },
+    { title: 'プロジェクト名', key: 'project' },
     { title: '説明', key: 'attribute.description' },
-    { title: '期間', key: 'period', sortable: false },
-    { title: '操作', key: 'actions', sortable: false },
+    { title: '期間', key: 'period', sortable: false, width: '150px' },
+    { title: '操作', key: 'actions', sortable: false, width: '180px' },
   ]
 
   const close = () => {
