d04c85b92e7e8dfa9c8620a048a58216d4f156f4
Murakami Hiroyuki <h.murakami@esm.co.jp>
2026-03-18 18:27:05 +0900

    feat: Delete associated Firebase Storage snapshots when a project is deleted.

--- packages/functions/src/scripts/deleteProject.ts
+++ packages/functions/src/scripts/deleteProject.ts
@@ -1,7 +1,16 @@
+import { getStorage } from 'firebase-admin/storage'
 import type { DeleteProject } from '../types/shared'
 import { prisma } from './common/commonFunctions'
 
 const deleteProject: DeleteProject = async (id) => {
+  // Firebase Storage の関連スナップショットを削除
+  const bucket = getStorage().bucket()
+  const prefix = `snapshots/${id}/`
+  const [files] = await bucket.getFiles({ prefix })
+  if (files.length > 0) {
+    await Promise.all(files.map((file) => file.delete()))
+  }
+
   await prisma.project.delete({
     where: { id },
   })
