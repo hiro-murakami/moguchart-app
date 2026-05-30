a6ff7066f0deddf0f69abff7d423d0bb4252af19
Murakami Hiroyuki <hiroyuki.m@gmail.com>
2026-05-30 09:05:49 +0900

    chore: update vite configuration settings for frontend package

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -10,7 +10,7 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: true,
+    sourcemap: 'hidden',
   },
   css: {
     devSourcemap: true,
